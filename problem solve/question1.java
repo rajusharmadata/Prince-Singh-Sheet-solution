e0841d7445dd5286863a33faf0a15419f9fec579
raju <rajusharmadata123@gmail.com>
2024-09-07 17:40:41 +0530

    add

---
+++ problem solve/question1.java
@@ -0,0 +1,23 @@
+
+/*  1. **Determining Even/Odd Numbers**  
+**Difficulty**: Easy  
+**Topics**: Basic Programming  
+**Description**: Write a program to check whether a number is even or odd.  
+**Example**:  
+Input: `number = 4`  
+Output: `Even`  
+Explanation: Since 4 is divisible by 2, it is an even number.  */
+import java.util.*;
+
+public class question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("odd ");
+        }
+    }
+}
