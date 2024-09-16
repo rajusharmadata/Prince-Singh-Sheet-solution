6664c9eb9d33d348c22ffe3ebb6e04485e3ba6d1
rajusharma <rajusharmadata123@gmail.com>
2024-09-17 02:58:42 +0530

    chnge my folder

--- problem solve/optimize solution/question6.java
+++ problem solve/optimize solution/question6.java
@@ -1,5 +1,31 @@
 
 import java.util.*;
 public class question6 {
-    
+    // uesing recorsen
+    public static boolean StringPlandromchek(int i,int j,String str){
+        // cheak the i is greter and equal then of j to retrn true
+
+        
+        if(i>=j){
+            return true;
+        }
+        if(str.charAt(i)!=str.charAt(j)){
+            return false;
+        }
+        // my base case is return statement
+        return (StringPlandromchek(i+1, j-1, str));
+    }
+    public static void main(String [] args){
+     Scanner sc = new Scanner(System.in);
+     String str = sc.nextLine();
+        int i = 0;
+        int j = str.length()-1;
+       if( StringPlandromchek(i, j, str)){
+        System.err.println("Planidorm");
+       }else {
+        System.err.println("not planidorm");
+       }
+
+    }
+
 }
